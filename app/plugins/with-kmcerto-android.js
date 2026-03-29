177d8d3fcf91a44aad2edd8a36657aefe0c5698f
camerabejamin-create <camerabejamin@gmail.com>
2026-03-29 18:02:41 -0300

    with-kmcerto-android.js

--- app/plugins/with-kmcerto-android.js
+++ app/plugins/with-kmcerto-android.js
@@ -1,26 +1,3 @@
-const { withAndroidManifest } = require("@expo/config-plugins");
-
 module.exports = function withKmcertoAndroid(config) {
-  config = withAndroidManifest(config, (config) => {
-    const manifest = config.modResults.manifest;
-    const permissions = [
-      "android.permission.SYSTEM_ALERT_WINDOW",
-      "android.permission.FOREGROUND_SERVICE",
-      "android.permission.FOREGROUND_SERVICE_SPECIAL_USE",
-    ];
-    if (!manifest["uses-permission"]) {
-      manifest["uses-permission"] = [];
-    }
-    for (const perm of permissions) {
-      const exists = manifest["uses-permission"].some(
-        (p) => p.$?.["android:name"] === perm
-      );
-      if (!exists) {
-        manifest["uses-permission"].push({ $: { "android:name": perm } });
-      }
-    }
-    return config;
-  });
-
   return config;
 };
