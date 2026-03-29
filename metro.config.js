26e98d12e568ea54190a67ea5874f7d24839440a
camerabejamin-create <camerabejamin@gmail.com>
2026-03-29 16:00:33 -0300

    metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,21 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+// Resolve local native modules
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "@/modules/kmcerto-native": path.resolve(__dirname, "modules/kmcerto-native"),
+};
+
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(__dirname, "modules"),
+];
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
 });
