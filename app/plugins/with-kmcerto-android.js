3a2a6510bdf6a21bda443705ff1c7f23566889d4
camerabejamin-create <camerabejamin@gmail.com>
2026-03-29 13:16:59 -0300

    with-kmcerto-android.js

--- app/plugins/with-kmcerto-android.js
+++ app/plugins/with-kmcerto-android.js
@@ -1,7 +1,3 @@
-const { withAndroidManifest } = require("@expo/config-plugins");
-
 module.exports = function withKmcertoAndroid(config) {
-  return withAndroidManifest(config, (config) => {
-    return config;
-  });
+  return config;
 };
